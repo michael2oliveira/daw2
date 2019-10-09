2edb1c5aa54c3b07b6e6e99c12092a916647ac96
michael2oliveira <55862120+michael2oliveira@users.noreply.github.com>
2019-10-09 20:31:52 -0300

    Update editar.php

--- dwa2/categoria/editar.php
+++ dwa2/categoria/editar.php
@@ -2,11 +2,11 @@
     
 
 
-  include_once '../class/adm.class.php';
+  include_once '../class/categoria.class.php';
   
-  $objadm = new adm();
-  $objadm->id_admis = $_GET['id_admis'];
-  $retorno = $objadm->retornaUnico();
+  $objcat = new cat();
+  $objcat->id_cate = $_GET['id_cate'];
+  $retorno = $objcat->retornaUnico();
   
   ?>
   
@@ -14,11 +14,8 @@
    
          Nome:  <input type= "text" name= "nome" value ="<?php echo $retorno->nome;?>" />
    
-         email:  <input type= "text" name= "email" value ="<?php echo $retorno->email;?>" />
-		 
-	   Senha:     <input type= "number" name= "senha" value ="<?php echo $retorno->senha;?>" />
           
-		  <input type= "hidden" name= "idee" value ="<?php echo $retorno->id_admis;?>" />
+		  <input type= "hidden" name= "idee" value ="<?php echo $retorno->id_cate;?>" />
 		  
           <input type = "submit" value ="Enviar"  />
    
