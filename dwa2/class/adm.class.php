1b3c46e25754a0e5dc8f859e5570ae2ff60eb476
michael2oliveira <55862120+michael2oliveira@users.noreply.github.com>
2019-10-16 21:37:51 -0300

    Add files via upload

---
+++ dwa2/class/adm.class.php
@@ -0,0 +1,170 @@
+ <?php
+ 
+ class adm { 
+	     
+		 private $id_admis;
+		 private $nome;
+		 private $email;
+		 private $senha;
+		
+		 
+		 private $conexao;
+		 private $tabela;
+		 
+		 public function __construct() {
+			
+            $this->conexao=mysqli_connect("localhost","root","","meubanc")	
+             or die ("erro na conexao")	;
+           
+		   $this->tabela = "admis";
+           
+		 } 		   
+		 
+		 
+		  public function __destruct() {
+			  
+           	unset($this->conexao);
+			
+		 }
+		 
+		 public function __get($nome){
+			 
+			 return $this->$nome;
+		 } 
+		 
+		 public function __set($nome,$value){
+		 
+	      $this->$nome = $value ;
+	    }
+        
+		  public function add() {
+			
+		    $sql= "INSERT INTO $this->tabela(nome,email,senha) VALUES ('$this->nome' , '$this->email' , '$this->senha')";
+		  
+		  
+		  $resultado = mysqli_query($this->conexao,$sql);
+		  
+		  return $resultado;
+		  
+		} 
+		  public function listar() { 
+		
+		     $sql = "SELECT * FROM $this->tabela";
+		  
+		    $resultado = mysqli_query($this->conexao,$sql); 
+			
+			$retorno = null;
+			
+			while ($res = mysqli_fetch_assoc($resultado) ) {
+				
+			  $objadm = new adm ();
+			  
+		      $objadm->id_admis= $res ['id_admis'];
+			  
+	          $objadm->nome = $res ['nome'];
+			  
+              $objadm->email = $res ['email']; 
+	  
+              $objadm->senha = $res ['senha'];
+       
+	       
+	            
+				
+				$retorno [] = $objadm;
+			
+			    
+			
+			} 
+			   return $retorno;
+			   
+			   
+		}   
+			 
+			
+	         public function retornaUnico(){
+				 
+	         $sql=  "SELECT * FROM $this->tabela where id_admis=$this->id_admis";
+			   
+			   $resultado = mysqli_query($this->conexao, $sql);
+			   $retorno = null;
+			   
+			   if($res = mysqli_fetch_assoc($resultado)) {
+				   
+				   $objadm = new adm();
+				   $objadm->nome = $res['nome'];
+				   $objadm->id_admis = $res['id_admis'];
+				   $objadm->email = $res ['email'];
+				   $objadm->senha = $res ['senha'];
+				   $retorno = $objadm;
+				   
+			   
+			   
+			   
+			   }
+
+                return $retorno;
+				
+		}
+			  public function editar() {
+
+               $sql = "UPDATE $this->tabela  SET nome = '$this->nome' , email = '$this->email' , senha = $this->senha  WHERE id_admis = $this->id_admis";
+			//  echo $sql;
+			   $resultado = mysqli_query($this->conexao,$sql);
+			   
+			   return $resultado;
+			   
+	    }   
+		
+                
+		   
+   		   public function excluir(){
+			   
+            $sql = "DELETE FROM $this->tabela WHERE id_admis = $this->id_admis";
+            $resultado = mysqli_query($this->conexao, $sql);
+            return $resultado;
+              
+        }  
+			
+			
+			
+			
+              			   
+			   
+            public function login(){
+				 
+	         $sql="SELECT * FROM $this->tabela WHERE email='$this->email' and senha ='$this->senha' and tipo = 'administrador'";
+			   
+			   echo $sql;
+			   
+			   $resultado = mysqli_query($this->conexao,$sql);
+			   $retorno = null;
+			   
+			   if($res = mysqli_fetch_assoc($resultado)) {
+				   
+				   $objUsuario = new usuario();
+				   $objUsuario->id_usuario = $res['id_usuario'];
+				   $objUsuario->nome = $res['nome'];
+				   $retorno = $objUsuario;
+			   
+			   
+			   } return $retorno;
+     			
+		
+	} 
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+?>
